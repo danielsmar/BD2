d7145b33df566fa94fedda79cd024c706f3cc078
Daniel Marculano <91577318+danielsmar@users.noreply.github.com>
2022-11-06 14:44:33 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -5,7 +5,7 @@ CREATE TABLE alunos
    ( codAluno    NUMBER(8) NOT NULL
    , nome        VARCHAR2(100) NOT NULL
    , nota        NUMBER(4,2)
-   , finalizado  CHAR(1) DEFAULT 'N'
+   , finalizado  CHAR(1) DEFAULT 'N' CONSTRAINT alunos_finalizado NOT NULL
   );
 
 ALTER TABLE alunos  
@@ -34,38 +34,26 @@ BEGIN
 END;
 /
 
-
---Criação do Trigger para  impedir a alteração/remoção de dados da tabela alunos caso finalizado seja 'S'
-CREATE OR REPLACE TRIGGER trg_alunos_finalizado
-BEFORE UPDATE OR DELETE ON ALUNOS
-FOR EACH ROW
-BEGIN
-    if :old.finalizado = 'S' THEN
-    raise_application_error(-20002, 'Impossível Alterar, o Aluno Está Finalizado');
-    end if;
-END;
-/
-
 INSERT INTO alunos(nome, nota) VALUES('Daniel Valadares Marculano',10);
 INSERT INTO alunos(nome, nota) VALUES('Andre Prado Bonitao',9.9);
 INSERT INTO alunos(nome, nota) VALUES('Joao Vitor Tarzan',8.5);
 INSERT INTO alunos(nome, nota) VALUES('Pedro Lima Corno',5);
 INSERT INTO alunos(nome, nota) VALUES('Yuri Alberto Lider',10);
 
-
 UPDATE alunos
 SET finalizado = 'S'
 WHERE codAluno = 20230001
 ;
-
-/*UPDATE alunos
-SET nome = 'Claudio da Silva'
+/*
+UPDATE alunos
+SET nome = 'CABRITO'
 WHERE codAluno = 20230001
 ;*/
+
 DROP TABLE usuarios CASCADE CONSTRAINTS;
 CREATE TABLE usuarios
-    (username   VARCHAR2(50) NOT NULL
-    ,permissao  VARCHAR2(1)  NOT NULL
+    (username   VARCHAR2(50) CONSTRAINT usuario_cargo_nn      NOT NULL
+    ,permissao  VARCHAR2(1)  CONSTRAINT usuarios_permissao_nn NOT NULL
     );
     
 ALTER TABLE usuarios
@@ -75,107 +63,13 @@ ALTER TABLE usuarios
 ADD CONSTRAINT usuarios_permissao_ck
 CHECK(permissao IN('C', 'A'));
 
-
-CREATE OR REPLACE TRIGGER trg_usuarios_permissao
-BEFORE UPDATE OR DELETE ON alunos
-DECLARE
-    permissao_sessao VARCHAR2(1);
-BEGIN
-SELECT 
-    permissao
-INTO
-    permissao_sessao
-FROM 
-    usuarios
-WHERE
-    username = user;
-    
-    IF permissao_sessao = 'C' and :old.finalizado = 'S' AND UPDATING THEN
-    raise_application_error(-20002, 'Você não tem permissao para alterar ou atualizar a tabela alunos.');
-    
-    ELSIF permissao_sessao = 'A' and :old.finalizado = 'S' AND DELETING THEN
-    raise_application_error(-20002, 'Você não pode deletar esse estudante.');
-    END IF;
-END;
-
-
--- Cria a tabela alunos com as restrições
-DROP TABLE alunos CASCADE CONSTRAINTS;
-CREATE TABLE alunos 
-   ( codAluno    NUMBER(8) NOT NULL
-   , nome        VARCHAR2(100) NOT NULL
-   , nota        NUMBER(4,2)
-   , finalizado  CHAR(1) DEFAULT 'N'
-  );
-
-ALTER TABLE alunos  
-ADD CONSTRAINT pk_alunos PRIMARY KEY(codAluno);
-
-ALTER TABLE alunos
-ADD CONSTRAINT alunos_finalizado_ck
-CHECK (finalizado IN ('S', 'N'));
-
-
---Criação da sequencia Alunos
-DROP SEQUENCE seq_alunos;
-CREATE SEQUENCE seq_alunos
-    START WITH 20230001
-    INCREMENT BY 1
-    MAXVALUE 20239999
-    MINVALUE 1
-;
-
---Criação do Trigger para inserir código autimaticamente
-CREATE OR REPLACE TRIGGER trg_alunos
-BEFORE INSERT ON alunos
-FOR EACH ROW
-BEGIN
-    :new.codAluno := seq_alunos.nextval;
-END;
-/
-
-
---Criação do Trigger para  impedir a alteração/remoção de dados da tabela alunos caso finalizado seja 'S'
-CREATE OR REPLACE TRIGGER trg_alunos_finalizado
-BEFORE UPDATE OR DELETE ON ALUNOS
-FOR EACH ROW
-BEGIN
-    if :old.finalizado = 'S' THEN
-    raise_application_error(-20002, 'Impossível Alterar, o Aluno Está Finalizado');
-    end if;
-END;
-/
-
-INSERT INTO alunos(nome, nota) VALUES('Daniel Valadares Marculano',10);
-INSERT INTO alunos(nome, nota) VALUES('Andre Prado Bonitao',9.9);
-INSERT INTO alunos(nome, nota) VALUES('Joao Vitor Tarzan',8.5);
-INSERT INTO alunos(nome, nota) VALUES('Pedro Lima Corno',5);
-INSERT INTO alunos(nome, nota) VALUES('Yuri Alberto Lider',10);
-
-
-UPDATE alunos
-SET finalizado = 'S'
-WHERE codAluno = 20230001
-;
-
-/*UPDATE alunos
-SET nome = 'Claudio da Silva'
-WHERE codAluno = 20230001
-;*/
-DROP TABLE usuarios CASCADE CONSTRAINTS;
-CREATE TABLE usuarios
-    (username   VARCHAR2(50) NOT NULL
-    ,permissao  VARCHAR2(1)  NOT NULL
-    );
-    
 ALTER TABLE usuarios
-ADD CONSTRAINT usuarios_pk PRIMARY KEy (username);
+ADD CONSTRAINT usuario_cargo_ck
+CHECK (username in('HR', 'ADMIN'));
 
-ALTER TABLE usuarios
-ADD CONSTRAINT usuarios_permissao_ck
-CHECK(permissao IN('C', 'A'));
+INSERT INTO usuarios(username, permissao) VALUES('ADMIN','A');
+INSERT INTO usuarios(username, permissao) VALUES('HR','C');
 
-/
 CREATE OR REPLACE TRIGGER trg_usuarios_permissao
 BEFORE UPDATE OR DELETE ON alunos
 FOR EACH ROW
@@ -191,65 +85,41 @@ FROM
 WHERE
     upper(username) = upper(user);
     
-    IF permissao_sessao = 'C' and :old.finalizado = 'S' AND UPDATING THEN
-    raise_application_error(-20002, 'Você não tem permissao para alterar ou atualizar a tabela alunos.');
+    IF permissao_sessao = 'C' and :old.finalizado = 'S'  THEN
+    raise_application_error(-20000, 'Você não tem permissao para alterar ou atualizar a tabela alunos.');
     
     ELSIF permissao_sessao = 'A' and :old.finalizado = 'S' AND DELETING THEN
     raise_application_error(-20002, 'Você não pode deletar esse estudante.');
     END IF;
 END;
+/
 
-CREATE USER administrador identified by 123456;
+-- CRIANDO USUARIOS
+DROP USER  "ADMIN" CASCADE;
+CREATE USER "ADMIN" IDENTIFIED BY "1234"  ;
 
-select * from usuarios;
+-- ROLES
+GRANT "DBA" TO "ADMIN" ;
+GRANT "CONNECT" TO "ADMIN" ;
+GRANT "RESOURCE" TO "ADMIN" ;
 
+-- SYSTEM PRIVILEGES
+GRANT CREATE ROLE TO "ADMIN" ;
+GRANT CREATE TRIGGER TO "ADMIN" ;
+GRANT ALTER SESSION TO "ADMIN" ;
+GRANT CREATE VIEW TO "ADMIN" ;
+GRANT CREATE SESSION TO "ADMIN" ;
+GRANT CREATE TABLE TO "ADMIN" ;
+GRANT ALTER USER TO "ADMIN" ;
+GRANT CREATE SEQUENCE TO "ADMIN" ;
+GRANT CREATE USER TO "ADMIN" ;
+GRANT DROP USER TO "ADMIN" ;
+GRANT ALTER SYSTEM TO "ADMIN" ;
+GRANT DROP TABLESPACE TO "ADMIN" ;
 
+CONNECT ADMIN/1234@localhost:1521/xepdb1
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-select * from usuarios;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+DELETE alunos 
+WHERE codAluno = 20230001 ;
 
 
