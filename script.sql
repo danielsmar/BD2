b606caf7714187be04b54a0d49e0bb02ca1ef8f1
Daniel Marculano <91577318+danielsmar@users.noreply.github.com>
2022-11-09 15:10:19 -0300

    Update script.sql

--- script.sql
+++ script.sql
@@ -1,28 +1,3 @@
--- CRIANDO USUARIOS
-DROP USER  "ADMIN" CASCADE;
-CREATE USER "ADMIN" IDENTIFIED BY "1234"  ;
-
--- ROLES
-GRANT "DBA" TO "ADMIN" ;
-GRANT "CONNECT" TO "ADMIN" ;
-GRANT "RESOURCE" TO "ADMIN" ;
-
--- SYSTEM PRIVILEGES
-GRANT CREATE ROLE TO "ADMIN" ;
-GRANT CREATE TRIGGER TO "ADMIN" ;
-GRANT ALTER SESSION TO "ADMIN" ;
-GRANT CREATE VIEW TO "ADMIN" ;
-GRANT CREATE SESSION TO "ADMIN" ;
-GRANT CREATE TABLE TO "ADMIN" ;
-GRANT ALTER USER TO "ADMIN" ;
-GRANT CREATE SEQUENCE TO "ADMIN" ;
-GRANT CREATE USER TO "ADMIN" ;
-GRANT DROP USER TO "ADMIN" ;
-GRANT ALTER SYSTEM TO "ADMIN" ;
-GRANT DROP TABLESPACE TO "ADMIN" ;
-
-CONNECT ADMIN/1234@localhost:1521/xepdb1
-
 -- Cria a tabela alunos com as restrições
 DROP TABLE alunos CASCADE CONSTRAINTS;
 CREATE TABLE alunos 
@@ -64,16 +39,6 @@ INSERT INTO alunos(nome, nota) VALUES('Joao Vitor Tarzan',8.5);
 INSERT INTO alunos(nome, nota) VALUES('Pedro Lima Corno',5);
 INSERT INTO alunos(nome, nota) VALUES('Yuri Alberto Lider',10);
 
-UPDATE alunos
-SET finalizado = 'S'
-WHERE codAluno = 20230001
-;
-/*
-UPDATE alunos
-SET nome = 'CABRITO'
-WHERE codAluno = 20230001
-;*/
-
 DROP TABLE usuarios CASCADE CONSTRAINTS;
 CREATE TABLE usuarios
     (username   VARCHAR2(50)  NOT NULL
@@ -110,7 +75,7 @@ WHERE
     upper(username) = upper(user);
     
     IF permissao_sessao = 'C' and :old.finalizado = 'S'  THEN
-    raise_application_error(-20000, 'Você não tem permissao para alterar ou atualizar a tabela alunos.');
+    raise_application_error(-20002, 'Você não tem permissao para alterar ou atualizar a tabela alunos.');
     
     ELSIF permissao_sessao = 'A' and :old.finalizado = 'S' AND DELETING THEN
     raise_application_error(-20002, 'Você não pode deletar esse estudante.');
@@ -118,6 +83,26 @@ WHERE
 END;
 /
 
-DELETE alunos 
-WHERE codAluno = 20230001 ;
+-- CRIANDO USUARIOS
+DROP USER  "ADMIN" CASCADE;
+CREATE USER "ADMIN" IDENTIFIED BY "1234";
+GRANT SELECT, UPDATE, INSERT, DELETE ON system.alunos to ADMIN;
+GRANT CREATE SESSION TO "ADMIN";
+
+--CONECTANDO AO USUARIO ADMIN
+CONNECT ADMIN/1234@localhost:1521/xepdb1
+
+select * from alunos ;
+
+
+UPDATE system.alunos
+SET finalizado = 'S'
+WHERE codAluno = 20230001
+;
+
+DELETE system.alunos
+WHERE codAluno = 20230001
+;
+
+
 
